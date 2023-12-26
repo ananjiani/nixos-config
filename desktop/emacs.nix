00065f390cd22a4155ce54f5d8a1d43f2aff9cc5
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2023-12-26 15:48:50 -0600

    automatically set up doom emacs

--- desktop/emacs.nix
+++ desktop/emacs.nix
@@ -1,21 +1,28 @@
 { config, pkgs, lib, ... }:
 
-let
-
-in
 {
   home.packages = with pkgs; [
-    ((emacsPackagesFor emacs-pgtk).emacsWithPackages (
+    ((emacsPackagesFor emacs29).emacsWithPackages (
       epkgs: [
         epkgs.org-roam
       ]
     ))
-  ]
+  ];
 
-  home.activation.installDoomEmacs =  ''
-    if [ ! -d "$XDG_CONFIG_HOME/emacs" ]; then
-      git clone --depth=1 "https://github.com/doomemacs/doomemacs" "$XDG_CONFIG_HOME/emacs"
-      $XDG_CONFIG_HOME/emacs/bin/doom install
-      export PATH=$XDG_CONFIG_HOME/emacs/bin:$PATH
-  '';
+  home.activation.installDoomEmacs = lib.hm.dag.entryAfter ["installPackages"] ''
+      if [ ! -d "$HOME/.emacs.d" ]; then
+        PATH="${config.home.path}/bin:$PATH"
+        git clone --depth=1 --single-branch https://github.com/doomemacs/doomemacs $HOME/.emacs.d
+        mkdir $HOME/.doom.d
+        cp $HOME/.emacs.d/templates/init.example.el $HOME/.doom.d/init.el
+        cp $HOME/.emacs.d/templates/config.example.el $HOME/.doom.d/config.el
+        cp $HOME/.emacs.d/templates/packages.example.el $HOME/.doom.d/packages.el
+        $HOME/.emacs.d/bin/doom sync
+        $HOME/.emacs.d/bin/doom env
+      fi
+    '';
+
+  home.sessionPath = [
+    "$HOME/.emacs.d/bin"
+  ];
 }
