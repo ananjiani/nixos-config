a9f90f1ab81fbdc0854a803da0f394ca24933c14
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2023-12-26 14:26:31 -0600

    emacs config

--- flake.nix
+++ flake.nix
@@ -5,12 +5,10 @@
     nixpkgs.url = "nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     hyprland = {
       url = "github:hyprwm/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
@@ -20,9 +18,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-colors.url = "github:misterio77/nix-colors";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
   };
 
-  outputs = {self, nixpkgs, chaotic, home-manager, hyprland, sddm-sugar-candy-nix, nix-colors, ...}:
+  outputs = {self, nixpkgs, chaotic, home-manager, hyprland, sddm-sugar-candy-nix, nix-colors, emacs-overlay, ...}:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
@@ -50,6 +49,7 @@
           inherit pkgs;
           modules = [
             hyprland.homeManagerModules.default
+            {nixpkgs.overlays = [emacs-overlay.overlay];}
             ./desktop/home.nix
           ];
           extraSpecialArgs = { inherit nix-colors; };
