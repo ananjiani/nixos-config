01865d2cc851f617b4679e5f0906172e28b06498
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-01-01 17:14:42 -0600

    modularize hyprland

---
+++ home/defaults.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, lib, ...}:
+
+{
+  xdg = {
+      configFile."mimeapps.list".force = true;
+      mimeApps = {
+        enable = true;
+
+        defaultApplications = {
+          "text/html" = "firefox.desktop";
+          "x-scheme-handler/http" = "firefox.desktop";
+          "x-scheme-handler/https" = "firefox.desktop";
+          "x-scheme-handler/about" = "firefox.desktop";
+          "x-scheme-handler/unknown" = "firefox.desktop";
+          "inode/directory" = "thunar.desktop";
+        };
+      };
+    };
+}
--- profiles/desktop/home.nix
+++ profiles/desktop/home.nix
@@ -1,14 +1,16 @@
-{ config, pkgs, lib, nix-colors, ... }:
+{ config, pkgs, lib, ... }:
+
 let
-  nix-colors-lib = nix-colors.lib.contrib {inherit pkgs; };
   wallpaper = ./wallpapers/revachol.jpg;
 in
 {
   imports =
-  [ # Include the results of the hardware scan.
+  [ 
     ../../home/emacs/emacs.nix
     ../../home/home-gaming.nix
-    nix-colors.homeManagerModules.default
+    ../../home/wm/wm.nix
+    ../../home/utils.nix
+    ../../home/defaults.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
@@ -25,165 +27,14 @@ in
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
-  # Theming
-  colorScheme = nix-colors.colorSchemes.gruvbox-material-dark-soft;
-#   colorScheme = nix-colors-lib.colorSchemeFromPicture {
-#     path = wallpaper;
-#     kind = "dark";
-#   };
 
-  gtk = {
-    enable = true;
-    theme= {
-      name = "gruvbox-dark";
-      package = pkgs.gruvbox-dark-gtk;
-    };
-    iconTheme.package = pkgs.gruvbox-dark-icons-gtk;
-    iconTheme.name = "Gruvbox-Dark";
-    };
-
-  qt = {
-    enable = true;
-    platformTheme = "gtk";
-    style.name = "adwaita-dark";
-#     style.name = "Gruvbox"
-  };
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    swaynotificationcenter
-    gparted
-    waybar
-  ];
-
-  home.file = with config.colorScheme.colors; {
-    ".config/waybar/config.jsonc".source = ../../home/wm/waybar/config.jsonc;
-    ".config/waybar/style.css".source = ../../home/wm/waybar/style.css;
-    ".config/swaync/config.json".source = ../../home/wm/swaync/config.json;
-    ".config/swaync/style.css".source = ../../home/wm/swaync/style.css;
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/ammar/etc/profile.d/hm-session-vars.sh
-  #
-
-  programs = {
-    fuzzel = {
-      enable = true;
-      settings = {
-        colors = with config.colorScheme.colors;{
-          background = "${base00}ff";
-          text = "${base05}ff";
-          match = "${base08}ff";
-          selection = "${base01}ff";
-          selection-text = "${base05}ff";
-          selection-match = "${base08}ff";
-          border = "${base09}ff";
-        };
-      };
-    };
-
-    bash = {
-      enable = true;
-    };
-    
-    git = {
-      enable = true;
-      userName = "Ammar Nanjiani";
-      userEmail = "ammar.nanjiani@gmail.com";
-    };
-
-    alacritty = {
-      enable = true;
-      settings = {
-            window = {
-            opacity = 0.8;
-            blur = true;
-          };
-        colors = with config.colorScheme.colors; {
-          bright = {
-            black = "0x${base00}";
-            blue = "0x${base0D}";
-            cyan = "0x${base0C}";
-            green = "0x${base0B}";
-            magenta = "0x${base0E}";
-            red = "0x${base08}";
-            white = "0x${base06}";
-            yellow = "0x${base09}";
-          };
-          cursor = {
-            cursor = "0x${base06}";
-            text = "0x${base06}";
-          };
-          normal = {
-            black = "0x${base00}";
-            blue = "0x${base0D}";
-            cyan = "0x${base0C}";
-            green = "0x${base0B}";
-            magenta = "0x${base0E}";
-            red = "0x${base08}";
-            white = "0x${base06}";
-            yellow = "0x${base0A}";
-          };
-          primary = {
-            background = "0x${base00}";
-            foreground = "0x${base06}";
-          };
-        }; 
-      };
-    };
-  };
-
-  wayland.windowManager.hyprland = with config.colorScheme.colors; {
-    enable = true;
-    extraConfig = ''
-      # See https://wiki.hyprland.org/Configuring/Monitors/
-      monitor=,highrr,auto,1,vrr,2
-      exec-once = corectrl
-      exec-once = swaybg -i ${wallpaper} -m fit
-      exec-once = ${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1
-      exec-once = gamescope -e -W 5120 -H 1440 -f -r 240 --adaptive-sync -- steam
-      general {
-        
-        # See https://wiki.hyprland.org/Configuring/Variables/ for more
-        gaps_in = 5
-        gaps_out = 20
-        border_size = 2
-        col.active_border = rgba(${base09}ee) rgba(${base0A}ee) 45deg
-        col.inactive_border = rgba(595959aa)
-        no_cursor_warps = true
-        layout = master
-
-        # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
-        allow_tearing = false
-      }
-    '' + builtins.readFile ../../home/wm/hyprland.conf;
-
-  };
-
-  xdg = {
-    configFile."mimeapps.list".force = true;
-    mimeApps = {
-      enable = true;
-
-      defaultApplications = {
-        "text/html" = "firefox.desktop";
-        "x-scheme-handler/http" = "firefox.desktop";
-        "x-scheme-handler/https" = "firefox.desktop";
-        "x-scheme-handler/about" = "firefox.desktop";
-        "x-scheme-handler/unknown" = "firefox.desktop";
-        "inode/directory" = "thunar.desktop";
-      };
-    };
+  wayland.windowManager.hyprland.settings = {
+      monitor = [",highrr,auto,1,vrr,2"];
+      exec-once = [
+        "corectrl"
+        "gamescope -e -W 5120 -H 1440 -f -r 240 --adaptive-sync -- steam"
+        "swaybg -i ${wallpaper} -m fit"
+      ];
   };
 
   # Let Home Manager install and manage itself.
