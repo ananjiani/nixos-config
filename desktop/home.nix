b5a1a4d9ea9e05062335764955897911712a2f81
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-01-01 14:27:01 -0600

    put gaming software into modules

--- desktop/home.nix
+++ desktop/home.nix
@@ -7,6 +7,7 @@ in
   imports =
   [ # Include the results of the hardware scan.
     ./emacs.nix
+    ./home-gaming.nix
     nix-colors.homeManagerModules.default
   ];
 
@@ -51,46 +52,16 @@ in
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    webcord
     swaynotificationcenter
     gparted
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want tobuiltins. install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = with config.colorScheme.colors; {
 
     ".config/waybar/config.jsonc".source = ../waybar/config.jsonc;
     ".config/waybar/style.css".source = ../waybar/style.css;
     ".config/swaync/config.json".source = ../swaync/config.json;
     ".config/swaync/style.css".source = ../swaync/style.css;
-
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
   # Home Manager can also manage your environment variables through
@@ -105,15 +76,10 @@ in
   #  /etc/profiles/per-user/ammar/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    DOOMDIR = "$HOME/.dotfiles/doom-emacs";
   };
 
   programs = {
-    obs-studio.enable = true;
-    mangohud = {
-      enable = true;
-    };
-
     fuzzel = {
       enable = true;
       settings = {
@@ -129,7 +95,9 @@ in
       };
     };
 
-    bash.enable = true;
+    bash = {
+      enable = true;
+    };
     
     git = {
       enable = true;
