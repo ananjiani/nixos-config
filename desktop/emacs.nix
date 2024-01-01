94c1f3602ce714b22bbc10b872508046f2eb6ded
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-01-01 14:29:42 -0600

    configure doomdir to point to this directory

--- desktop/emacs.nix
+++ desktop/emacs.nix
@@ -7,15 +7,16 @@
   };
 
   services.emacs.enable = true;
+  
+  home.sessionVariables = {
+    DOOMDIR = "$HOME/.dotfiles/doom-emacs";
+  };
 
   home.activation.installDoomEmacs = lib.hm.dag.entryAfter ["installPackages"] ''
       if [ ! -d "$HOME/.emacs.d" ]; then
         PATH="${config.home.path}/bin:$PATH"
+        export DOOMDIR="$HOME/.dotfiles/doom-emacs"
         git clone --depth=1 --single-branch https://github.com/doomemacs/doomemacs $HOME/.emacs.d
-        mkdir $HOME/.doom.d
-        cp $HOME/.emacs.d/templates/init.example.el $HOME/.doom.d/init.el
-        cp $HOME/.emacs.d/templates/config.example.el $HOME/.doom.d/config.el
-        cp $HOME/.emacs.d/templates/packages.example.el $HOME/.doom.d/packages.el
         $HOME/.emacs.d/bin/doom sync
         $HOME/.emacs.d/bin/doom env
       fi
@@ -25,12 +26,6 @@
       PATH="${config.home.path}/bin:$PATH"
       $HOME/.emacs.d/bin/doom sync -e
     '';
-
-  home.file = {
-    ".doom.d/config.el".source = ../doom-emacs/config.el;
-    ".doom.d/packages.el".source = ../doom-emacs/packages.el;
-    ".doom.d/init.el".source = ../doom-emacs/init.el;
-  };
 
   home.sessionPath = [
     "$HOME/.emacs.d/bin"
