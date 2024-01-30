6cfb14b44efd6f43d44112a8d2704d5c97bff294
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-01-29 23:11:58 -0600

    nvim config

--- home/nvim.nix
+++ home/nvim.nix
@@ -1,7 +1,10 @@
-{ config, pkgs, lib, nixvim, ...}:
-
-
 {
+  config,
+  pkgs,
+  lib,
+  nixvim,
+  ...
+}: {
   imports = [
     nixvim.homeManagerModules.nixvim
   ];
@@ -18,36 +21,70 @@
       enable = true;
     };
 
+    options = {
+      number = true;
+      relativenumber = true;
+      signcolumn = "number";
+    };
+
     clipboard = {
       providers.wl-copy.enable = true;
       register = "unnamedplus";
     };
 
     plugins = {
+      nix.enable = true;
+      undotree.enable = true;
+      harpoon.enable = true;
       treesitter = {
-      	enable = true;
-	indent = true;
-	nixvimInjections = true;
+        enable = true;
+        indent = true;
+        nixvimInjections = true;
       };
       telescope.enable = true;
       # codeium = {
       # 	enable = true;
       #   wrapper = "";
       # };
+      lsp-format.enable = true;
+      lspkind.enable = true;
+      dap = {
+        enable = true;
+        extensions = {
+          dap-python.enable = true;
+          dap-ui.enable = true;
+        };
+      };
+      none-ls = {
+        enable = true;
+        enableLspFormat = true;
+        sources = {
+          code_actions = {
+            statix.enable = true;
+          };
+          diagnostics = {
+            mypy.enable = true;
+          };
+          formatting = {
+            alejandra.enable = true;
+          };
+        };
+      };
       lsp = {
         enable = true;
         servers = {
+          pyright.enable = true;
+          ruff-lsp.enable = true;
           nil_ls.enable = true;
-	  nushell.enable = true;
-	  ruff-lsp.enable = true;
-	  yamlls.enable = true;
+          nushell.enable = true;
+          yamlls.enable = true;
         };
       };
     };
   };
   # programs = {
-  #   nvim = let 
-  #     toLua = str: "lua << EOF\n${str}\nEOF\n"; 
+  #   nvim = let
+  #     toLua = str: "lua << EOF\n${str}\nEOF\n";
   #     toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
   #   in {
   #     enable = true;
@@ -58,7 +95,7 @@
   #     defaultEditor = true;
 
   #     plugins = with pkgs.vimPlugins; [
-        
+
   #     ];
   #   };
   # };
