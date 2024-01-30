b43a81bb1c6f3534fec70eec0378983c3341506b
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-01-30 14:16:47 -0600

    helix

---
+++ home/helix.nix
@@ -0,0 +1,70 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+
+  programs.helix = {
+    enable = true;
+    
+    languages.language = [
+      {
+        name = "nix";
+        formatter.command = "nixpkgs-fmt";
+      }
+      {
+        name = "python";
+        roots = ["pyproject.toml"];
+        language-servers = ["pyright" "ruff"];
+      }
+    ];
+
+    languages.language-server = {
+      pyright = {
+        command = "pyright-langserver";
+        args = ["--stdio"];
+      };
+      ruff = {
+        command = "ruff-lsp";
+        config.settings.run = "onSave" ;
+      };
+    };    
+    settings = {
+      theme = "gruvbox_dark_hard";
+      editor = {
+        line-number = "relative";
+        auto-save = true;
+        bufferline = "multiple";
+        cursorline = true;
+        rulers = [120];
+        true-color = true;
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+        lsp = {
+          display-messages = true;
+        };
+        statusline.left = ["mode" "spinner" "version-control" "file-name"];
+      };
+      keys.normal = {
+        esc = ["collapse_selection" "keep_primary_selection"];
+      };
+    };
+
+    extraPackages = with pkgs; [
+      nil
+      nixpkgs-fmt
+      nodePackages.pyright
+      nodePackages.vscode-json-languageserver
+      taplo
+      taplo-cli
+      taplo-lsp
+      ruff
+      ruff-lsp
+      yaml-language-server
+    ];
+  };
+}
