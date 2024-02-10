ad167ad91bdaf50ad1f2d4a25b14be4eee58ddc1
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-02-09 18:07:45 -0600

    refile fzf

--- home/utils.nix
+++ home/utils.nix
@@ -67,23 +67,6 @@
       package = pkgs.nushellFull;
     };
 
-    fzf = {
-      enable = true;
-      colors = {
-        fg = "#ebdbb2";
-        bg = "#282828";
-        hl = "#fabd2f";
-        "fg+" = "#ebdbb2";
-        "bg+" = "#3c3836";
-        "hl+" = "#fabd2f";
-        info = "#83a598";
-        prompt = "#bdae93";
-        spinner = "#fabd2f";
-        pointer = "#83a598";
-        marker = "#fe8019";
-        header = "#665c54";
-      };
-    };
 
     bat.enable = true;
 
