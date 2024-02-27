57d33f226d9f1a87c4c21b3adefb3786689dedee
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-02-27 12:10:47 -0600

    extra monitors

--- profiles/desktop/home.nix
+++ profiles/desktop/home.nix
@@ -29,7 +29,11 @@ in
 
 
   wayland.windowManager.hyprland.settings = {
-      monitor = [",highrr,auto,1,vrr,2"];
+      monitor = [
+      "DP-2,5120x1440@240,0x1440,1,vrr,2"
+      "DP-1,3840x2160@60,2560x360,2"
+      "HDMI-A-1,2560x1440@60,0x0,1"
+      ];
       general.layout = "dwindle";
       exec-once = [
         "corectrl"
