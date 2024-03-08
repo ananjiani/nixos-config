71f98497823a1fd398f06eefd37178274cc60655
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-03-08 16:26:48 -0600

    exwm

--- home/emacs/doom-emacs/packages.el
+++ home/emacs/doom-emacs/packages.el
@@ -40,6 +40,7 @@
 ;; Use `:pin' to specify a particular commit to install. (package!
 ;; builtin-package :pin "1a2b3c4d5e")
 
+(package! exwm :disable t)
 (package! org-super-agenda)
 (package! org-ql)
 (package! olivetti)
--- home/wm/exwm.nix
+++
@@ -1,5 +0,0 @@
-{config, pkgs, lib, ...}:
-{
-  services.xserver.windowManager.exwm.enable = true;
-
-}
