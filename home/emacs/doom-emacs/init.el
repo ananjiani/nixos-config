61918aaac06a9be0ba2ead8a6ee2d200a1c36ddb
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-03-13 15:00:35 -0500

    emacs stuff

--- home/emacs/doom-emacs/init.el
+++ home/emacs/doom-emacs/init.el
@@ -21,7 +21,7 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company ; the ultimate code completion backend
+       company      ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
@@ -37,7 +37,7 @@
        hl-todo                ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        indent-guides            ; highlighted indent columns
-       (ligatures +extra +fira) ; ligatures and symbols to make your code pretty again
+       (ligatures +fira) ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline    ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
@@ -160,7 +160,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       ;; (python +lsp)
+       (python +lsp +pyright)
                                         ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
