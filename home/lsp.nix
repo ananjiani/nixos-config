facd2c7c139d07aeee4b341763c848c7ab73ae78
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-03-13 15:00:18 -0500

    python lsp server

--- home/lsp.nix
+++ home/lsp.nix
@@ -1,5 +1,9 @@
 { config, pkgs, lib, ... }: {
 
-  home.packages = with pkgs; [ nil ];
+  home.packages = with pkgs; [ 
+    nil
+    nodePackages.pyright
+    python311Packages.python-lsp-server
+  ];
 
 }
