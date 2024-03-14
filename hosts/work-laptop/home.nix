f3e77bdb067e305bba26706f56abf229fd03ece2
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-03-14 12:11:50 -0500

    change paths to reflect new structure

--- hosts/work-laptop/home.nix
+++ hosts/work-laptop/home.nix
@@ -1,36 +1,16 @@
 { config, pkgs, lib, ... }:
 let
-  wallpaper = ./wallpapers/revachol.png;
+  wallpaper = ../default/wallpapers/revachol.png;
 in
 {
 
   imports =
-  [ 
-    ../../home/emacs/emacs.nix
-    ../../home/wm/wm.nix
-    ../../home/utils.nix
-    ../../home/defaults.nix
+  [
+    ../default/home.nix
   ];
-
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "ammar";
-  home.homeDirectory = "/home/ammar";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
 
   services.blueman-applet.enable = true;
   services.mpris-proxy.enable = true;
-
-  # home.packages = with pkgs; [
-  # ];
 
   wayland.windowManager.hyprland.settings = {
     monitor = [
@@ -43,12 +23,8 @@ in
       "2, monitor:DP-4"
       "3, monitor:DP-3"
     ];
-    general.layout = "dwindle";
     exec-once = [
         "swaybg -i ${wallpaper} -m fill"
     ];
   };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
