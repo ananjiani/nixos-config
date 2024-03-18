a281a6af23b902c335d2324f078549adacc2e46e
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-03-17 21:12:53 -0500

    format

--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -1,14 +1,12 @@
 { config, pkgs, lib, ... }:
 
 {
-    home.packages = with pkgs; [
-        webcord
-        r2modman
-        gpu-screen-recorder
-        gpu-screen-recorder-gtk
-    ];
+  home.packages = with pkgs; [
+    webcord
+    r2modman
+    gpu-screen-recorder
+    gpu-screen-recorder-gtk
+  ];
 
-    programs = {
-        mangohud.enable = true;
-    };
+  programs = { mangohud.enable = true; };
 }
