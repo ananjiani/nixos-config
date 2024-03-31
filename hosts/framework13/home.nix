9dcbcc46476ac8ae95352bb07e964b209701bc3c
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-03-31 14:59:03 -0500

    framework13

---
+++ hosts/framework13/home.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, lib, ... }:
+let
+  wallpaper = ../default/wallpapers/revachol.png;
+in
+{
+
+  imports =
+  [
+    ../default/home.nix
+  ];
+
+  services.blueman-applet.enable = true;
+  services.mpris-proxy.enable = true;
+
+  wayland.windowManager.hyprland.settings = {
+    exec-once = [
+        "swaybg -i ${wallpaper} -m fill"
+    ];
+  };
+}
