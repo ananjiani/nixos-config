0ef2241f44e62f760dd617197b53cc06a3d9a0c2
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-03-31 13:29:12 -0500

    python debugging

--- modules/home/dev/lang/python.nix
+++ modules/home/dev/lang/python.nix
@@ -2,8 +2,11 @@
 
 {
   home.packages = with pkgs; [
+    python311Full
     nodePackages.pyright
     python311Packages.python-lsp-server
+    ruff
     ruff-lsp
+    python311Packages.debugpy
   ];
 }
