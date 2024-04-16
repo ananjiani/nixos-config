d5cfac63ad7c28a6ea83c13eef6c352fd3771417
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-04-15 19:46:18 -0500

    signal

--- hosts/framework13/home.nix
+++ hosts/framework13/home.nix
@@ -1,23 +1,15 @@
 { config, pkgs, lib, ... }:
-let
-  wallpaper = ../default/wallpapers/revachol.png;
-in
-{
+let wallpaper = ../default/wallpapers/revachol.png;
+in {
 
-  imports =
-  [
-    ../default/home.nix
-  ];
+  imports = [ ../default/home.nix ];
 
   services.blueman-applet.enable = true;
   services.mpris-proxy.enable = true;
 
+  home.packages = with pkgs; [ signal-desktop ];
   wayland.windowManager.hyprland.settings = {
-    monitor = [
-    ",preferred,auto,1"
-    ];
-    exec-once = [
-        "swaybg -i ${wallpaper} -m fill"
-    ];
+    monitor = [ ",preferred,auto,1" ];
+    exec-once = [ "swaybg -i ${wallpaper} -m fill" ];
   };
 }
