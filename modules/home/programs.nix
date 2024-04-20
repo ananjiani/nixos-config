880bab5b53573ff4849a0f37a1477b6c0519a7ac
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-04-20 14:33:55 -0500

    obs

--- modules/home/programs.nix
+++ modules/home/programs.nix
@@ -7,6 +7,10 @@
     xfce.thunar-archive-plugin
     xfce.thunar-media-tags-plugin
     obs-studio
+    gst_all_1.gstreamer
+    obs-studio-plugins.obs-gstreamer
+    obs-studio-plugins.obs-vkcapture
+    libva-utils
     cmatrix
     ungoogled-chromium
   ];
