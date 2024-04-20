9eb265feb57b56a903755e355204ac02e295a0cd
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-04-20 14:33:50 -0500

    gamescope (still doesn't work)

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -1,34 +1,32 @@
 { config, pkgs, lib, pkgs-stable, ... }:
 
 {
-
   programs = {
     steam = {
       enable = true;
       package = pkgs.steam.override {
-        extraEnv = {};
-        extraLibraries = pkgs: with pkgs; [
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXinerama
-          xorg.libXScrnSaver
-          libpng
-          libpulseaudio
-          libvorbis
-          stdenv.cc.cc.lib
-          libkrb5
-          keyutils
-        ];
+        extraEnv = { };
+        extraLibraries = pkgs:
+          with pkgs; [
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXinerama
+            xorg.libXScrnSaver
+            libpng
+            libpulseaudio
+            libvorbis
+            stdenv.cc.cc.lib
+            libkrb5
+            keyutils
+          ];
       };
     };
     gamemode.enable = true;
     gamemode.enableRenice = true;
   };
 
-  environment.systemPackages = 
-    (with pkgs; [
-      steamtinkerlaunch
-      lutris
-      gamescope
-    ]);
+  programs.gamescope.enable = true;
+  programs.gamescope.capSysNice = true;
+
+  environment.systemPackages = (with pkgs; [ steamtinkerlaunch lutris ]);
 }
