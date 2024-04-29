9f708823780c8532ae4dd03fbb508fcb92c3d927
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-04-29 12:04:27 -0500

    update kernel

--- modules/nixos/wm.nix
+++ modules/nixos/wm.nix
@@ -1,21 +1,21 @@
 { config, pkgs, lib, ... }:
 
 {
-  services.xserver.displayManager.sddm.sugarCandyNix = {
-    enable = true;
-    settings = {
-      Background =
-        lib.cleanSource ../../hosts/default/wallpapers/revachol-horse.jpg;
-      ScaleImageCropped = false;
-      ScreenWidth = 5120;
-      ScreenHeight = 1440;
-      FormPosition = "center";
-      FullBlur = true;
-      ForceHideCompletePassword = true;
-      HeaderText = "";
-      DimBackgroundImage = 0.3;
-    };
-  };
+  # services.displayManager.sddm.sugarCandyNix = {
+  #   enable = true;
+  #   settings = {
+  #     Background =
+  #       lib.cleanSource ../../hosts/default/wallpapers/revachol-horse.jpg;
+  #     ScaleImageCropped = false;
+  #     ScreenWidth = 5120;
+  #     ScreenHeight = 1440;
+  #     FormPosition = "center";
+  #     FullBlur = true;
+  #     ForceHideCompletePassword = true;
+  #     HeaderText = "";
+  #     DimBackgroundImage = 0.3;
+  #   };
+  # };
 
   # environment.sessionVariables = {
   #   NIXOS_OZONE_WL = "1";
