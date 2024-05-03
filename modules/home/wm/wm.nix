60af5e4659336d29808b3af0546f87791558f3e9
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-05-02 21:24:22 -0500

    waybar update

--- modules/home/wm/wm.nix
+++ modules/home/wm/wm.nix
@@ -1,18 +1,7 @@
-{
-  config,
-  pkgs,
-  lib,
-  inputs,
-  ...
-}: {
-  home.packages = with pkgs; [
-    swaynotificationcenter
-    waybar
-  ];
+{ config, pkgs, lib, inputs, ... }: {
+  home.packages = with pkgs; [ swaynotificationcenter ];
 
-  imports = [
-    inputs.nix-colors.homeManagerModules.default
-  ];
+  imports = [ inputs.nix-colors.homeManagerModules.default ];
 
   colorScheme = inputs.nix-colors.colorSchemes.gruvbox-material-dark-soft;
   #   colorScheme = nix-colors-lib.colorSchemeFromPicture {
@@ -31,8 +20,6 @@
   };
 
   home.file = {
-    ".config/waybar/config.jsonc".source = ../../home/wm/waybar/config.jsonc;
-    ".config/waybar/style.css".source = ../../home/wm/waybar/style.css;
     ".config/swaync/config.json".source = ../../home/wm/swaync/config.json;
     ".config/swaync/style.css".source = ../../home/wm/swaync/style.css;
   };
@@ -52,21 +39,28 @@
         };
       };
     };
+    waybar = {
+      enable = true;
+      style = ../../home/wm/waybar/style.css;
+      settings.mainBar =
+        builtins.fromJSON (builtins.readFile ../../home/wm/waybar/config.json);
+      package = inputs.waybar.packages.x86_64-linux.waybar;
+    };
   };
 
-  home.activation.reloadHyprland = lib.hm.dag.entryAfter ["reloadSystemd"] ''
+  home.activation.reloadHyprland = lib.hm.dag.entryAfter [ "reloadSystemd" ] ''
     hyprctl reload
   '';
 
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
-      exec = [
-        "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
-      ];
+      exec =
+        [ "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1" ];
       general = {
         layout = "dwindle";
-        "col.active_border" = with config.colorScheme.colors; "rgba(${base09}ee) rgba(${base0A}ee) 45deg";
+        "col.active_border" = with config.colorScheme.colors;
+          "rgba(${base09}ee) rgba(${base0A}ee) 45deg";
       };
       "$mainMod" = "SUPER";
       bind = [
