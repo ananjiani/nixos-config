90a42688ff29a3dca9791536930dbf52784a8a5d
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-05-22 16:50:06 -0500

    stuff

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -14,7 +14,7 @@ in
   wayland.windowManager.hyprland.settings = {
       monitor = [
       "DP-2,5120x1440@240,0x1440,1,vrr,2"
-      "DP-1,3840x2160@60,2560x360,2"
+      "DP-1,3840x2160@60,2560x0,1.5"
       "HDMI-A-1,2560x1440@60,0x0,1"
       ];
       exec-once = [
