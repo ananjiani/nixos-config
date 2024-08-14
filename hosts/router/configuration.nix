773250241556c2cad80db1e265aa35ee90739ff8
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-08-14 18:01:14 -0500

    proxmox

--- hosts/router/configuration.nix
+++ hosts/router/configuration.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 {
   imports = [ ./hardware-configuration.nix ../../modules/nixos/utils.nix ];
@@ -8,5 +8,6 @@
     enable = true;
     passwordAuthentication = true;
   };
-
+  services.proxmox-ve.enable = true;
+  nixpkgs.overlays = [ inputs.proxmox-nixos.overlays.x86_64-linux ];
 }
