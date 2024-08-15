5a0dee73fa83ea3e70768d9e3a1b182b8a5a5a90
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2024-08-15 18:36:51 -0500

    get rid of password authentication

--- hosts/router/configuration.nix
+++ hosts/router/configuration.nix
@@ -4,10 +4,7 @@
   imports = [ ./hardware-configuration.nix ../../modules/nixos/utils.nix ];
 
   networking.hostName = "router";
-  services.openssh = {
-    enable = true;
-    PasswordAuthentication = true;
-  };
+  services.openssh = { enable = true; };
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMoo8KQiLBJ6WrWmG0/6O8lww/v6ggPaLfv70/ksMZbD ammar.nanjiani@gmail.com"
   ];
