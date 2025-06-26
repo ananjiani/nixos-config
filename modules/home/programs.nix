193a05a9acaab0f4ac9bacd9dc807324d65a73f8
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2025-06-26 14:44:04 -0500

    writing update

--- modules/home/programs.nix
+++ modules/home/programs.nix
@@ -1,34 +1,32 @@
-{ config, pkgs, pkgs-stable, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
-let
-  tex = (pkgs.texlive.combine {
-    inherit (pkgs.texlive)
-      scheme-medium dvisvgm dvipng # for preview and export as html
-      wrapfig amsmath ulem hyperref capt-of biblatex biber biblatex-chicago
-      footmisc ragged2e titlesec geometry xcolor;
-    #(setq org-latex-compiler "lualatex")
-    #(setq org-preview-latex-default-process 'dvisvgm)
-  });
-in {
-  home.packages = (with pkgs; [
-    tex
-    xcolor
-    inkscape
-    pinta
-    vlc
-    xfce.thunar
-    xfce.thunar-archive-plugin
-    xfce.thunar-media-tags-plugin
-    obs-studio
-    gst_all_1.gstreamer
-    obs-studio-plugins.obs-gstreamer
-    obs-studio-plugins.obs-vkcapture
-    libva-utils
-    cmatrix
-    ungoogled-chromium
-    element-desktop
-    xournalpp
-  ])
+{
+  home.packages = (
+    with pkgs;
+    [
+      xcolor
+      inkscape
+      pinta
+      vlc
+      xfce.thunar
+      xfce.thunar-archive-plugin
+      xfce.thunar-media-tags-plugin
+      obs-studio
+      gst_all_1.gstreamer
+      obs-studio-plugins.obs-gstreamer
+      obs-studio-plugins.obs-vkcapture
+      libva-utils
+      cmatrix
+      ungoogled-chromium
+      element-desktop
+      xournalpp
+      openai-whisper
+    ]
+  );
 
-    ++ (with pkgs-stable; [ zotero ]);
 }
