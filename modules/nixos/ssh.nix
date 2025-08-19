a809a3b597b2c90eeb94a9ca56e8829fe29f8642
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2025-08-18 23:45:22 -0500

    fail2ban

--- modules/nixos/ssh.nix
+++ modules/nixos/ssh.nix
@@ -15,6 +15,24 @@ _:
     };
   };
 
+  # Enable fail2ban for SSH protection
+  services.fail2ban = {
+    enable = true;
+    bantime = "1h"; # Ban IPs for 1 hour
+    bantime-increment = {
+      enable = true; # Increase ban time for repeat offenders
+      multipliers = "1 2 4 8 16 32 64";
+      maxtime = "168h"; # Maximum ban time: 1 week
+    };
+    maxretry = 5; # Allow 5 failed attempts
+    ignoreIP = [
+      "127.0.0.1/8" # Localhost
+      "10.0.0.0/8" # Private networks
+      "172.16.0.0/12"
+      "192.168.0.0/16"
+    ];
+  };
+
   # Centralized authorized keys management
   # These keys can SSH INTO any machine that imports this module
   users.users.ammar.openssh.authorizedKeys.keys = [
