92ea30dda6417974758ece099115bae1694c6bfe
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2025-11-02 16:55:00 -0600

    feat: add Nitrokey and Bitwarden support

--- modules/nixos/privacy.nix
+++ modules/nixos/privacy.nix
@@ -5,10 +5,17 @@
 
 {
 
-  environment.systemPackages = with pkgs; [ tor-browser ];
+  environment.systemPackages = with pkgs; [
+    tor-browser
+    nitrokey-app2
+    bitwarden
+    bitwarden-cli
+  ];
   services.mullvad-vpn = {
     enable = true;
     package = pkgs.mullvad-vpn;
     enableExcludeWrapper = true;
   };
+
+  hardware.nitrokey.enable = true;
 }
