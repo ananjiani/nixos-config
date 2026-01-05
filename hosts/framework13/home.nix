395abda676a2a8ae560dc41cbd29921ce4b396d8
Ammar Nanjiani <ammar.nanjiani@gmail.com>
2026-01-05 15:56:56 -0600

    set up for laptop

--- hosts/framework13/home.nix
+++ hosts/framework13/home.nix
@@ -19,7 +19,10 @@
   services.blueman-applet.enable = true;
   services.mpris-proxy.enable = true;
 
-  home.packages = with pkgs; [ signal-desktop ];
+  home.packages = with pkgs; [
+    signal-desktop
+    moonlight-qt
+  ];
   wayland.windowManager.hyprland.settings = {
     monitor = [
       "eDP-1,2256x1504@60,0x0,1.175"
